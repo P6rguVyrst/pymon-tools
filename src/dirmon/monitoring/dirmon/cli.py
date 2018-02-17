adab79367901bad3ada2f0fe297c3093ff9e82d1
Toomas Ormisson <toomas.ormisson@gmail.com>
2018-02-18 00:38:30 +0200

    Adding Redis support to dirmon.

--- src/dirmon/monitoring/dirmon/cli.py
+++ src/dirmon/monitoring/dirmon/cli.py
@@ -7,12 +7,41 @@ import logging
 from watchdog.observers import Observer
 from watchdog.events import LoggingEventHandler
 import click
+import redis
 
+ class RedisHandler(FileSystemEventHandler):
+
+
+     def __init__(self, observer):
+         self.observer = observer
+         self.redis = redis.Redis()
+
+     def on_created(self, event):
+         self.rpush('queue', event)
+         print(event)
+     def on_moved(self, event):
+         self.rpush('queue', event)
+         print(event)
+     def on_deleted(self, event):
+         self.rpush('queue', event)
+         print(event)
+     def on_modified(self, event):
+         self.rpush('queue', event)
+         print(event)
+
+
+def choose_handler(handler, observer):
+    options = {
+        'logging': LoggingEventHandler,
+        'redis': RedisHandler,
+    }
+    return options.get(handler, 'logging')(observer)
 
 
 @click.command()
 @click.option('--path', '-p', default='.', help='System path to monitor')
 @click.option('--config', '-c', help='Configuration file')
+@click.option('--handler'. '-h', default='logging')
 def main(path, **kwargs):
     """Console script for dirmon."""
     conf = kwargs.get('config')
@@ -22,8 +51,11 @@ def main(path, **kwargs):
     logging.config.fileConfig(conf)
     logging.getLogger('dirmon')
 
-    event_handler = LoggingEventHandler()
     observer = Observer()
+    choose_handler(handler, observer)
+    #event_handler = LoggingEventHandler()
+    #event_handler = RedisHandler(observer)
+
     observer.schedule(event_handler, path, recursive=True)
     observer.start()
     try:
@@ -32,7 +64,6 @@ def main(path, **kwargs):
     except KeyboardInterrupt:
         observer.stop()
     observer.join()
-
 
 
 if __name__ == "__main__":
